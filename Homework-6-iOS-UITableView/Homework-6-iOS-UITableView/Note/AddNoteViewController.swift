39f1378664dcf8fed65e5cde3d4398f825506001
AndreiSavchenko <andrei.savchenko.ad@gmail.com>
2018-12-19 15:41:55 +0200

    Help

--- Homework-6-iOS-UITableView/Homework-6-iOS-UITableView/Note/AddNoteViewController.swift
+++ Homework-6-iOS-UITableView/Homework-6-iOS-UITableView/Note/AddNoteViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class AddNoteViewController: UIViewController {
+    var newNote: Note?
+    
     @IBOutlet weak var titleTextField: UITextField!
     
     @IBOutlet weak var descriptionTextView: UITextView!
@@ -17,14 +19,45 @@ class AddNoteViewController: UIViewController {
     
     @IBOutlet weak var completedSwitch: UISwitch!
     
+    
+    
+    func datePicker() -> String {
+        dateDatePicker.datePickerMode = UIDatePicker.Mode.date
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd MMM yyyy"
+        let selectedDate = dateFormatter.string(from: dateDatePicker.date)
+        return selectedDate
+    }
+    
+    struct Note {
+        var title = String()
+        var description = String()
+        var createdDate = String()
+        var completed = Bool()
+    }
+    
     @IBAction func saveBarButtonItem(_ sender: UIBarButtonItem) {
+        newNote = Note.init(title: titleTextField.text!,
+                            description: descriptionTextView.text,
+                            createdDate: datePicker(),
+                            completed: completedSwitch.isOn)
+        print(newNote as Any)
+        navigationController?.popToRootViewController(animated: true)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        descriptionTextView.layer.borderColor =  UIColor.lightGray.cgColor
+        descriptionTextView.layer.borderWidth = 1.0
+        descriptionTextView.layer.cornerRadius = 8
+        
     }
     
-
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "toMain" {
+//            let controller = (segue.destination as? UINavigationController)?.topViewController as? TableNoteViewController
+//            controller?.newNoteArr[]
+//        }
+//    }
 }
--- Homework-6-iOS-UITableView/Homework-6-iOS-UITableView/Note/TableNoteViewController.swift
+++ Homework-6-iOS-UITableView/Homework-6-iOS-UITableView/Note/TableNoteViewController.swift
@@ -12,61 +12,65 @@ class TableNoteViewController: UIViewController {
     
     @IBOutlet weak var noteTableView: UITableView!
     
-    let titleArr: [String] = ["Альф про любовь","Альф про еду","Советы от Альфа"]
-    let descriptionLoveArr: [String] = ["Ей пришла в голову глупейшая мысль, что она меня больше не любит.",
-                                        "Боюсь, тебе придется любить меня до гроба.",
-                                        "Я вас, люди, не понимаю. Если вы любите кого-то, вы боитесь, чтоб он узнал об этом. Но, когда оказывается, что вы ему тоже небезразличны, вы жалеете о потерянном времени. А если завтра ваша планета взорвется?"]
-    let descriptionFoodArr: [String] = ["Твои веки тяжелеют... Ты засыпаешь... Ты больше не кот... Ты бублик.",
-                                        "Овощи — это не еда, это то, что едят с едой!",
-                                        "Вы не любите кошек? Да вы просто не умеете их готовить!"]
-    let descriptionTipsArr: [String] = ["Лин, послушай старших: оставь искусство! Действуй по плану «А» — хватайся за первого попавшегося богача.",
-                                        "Ой-ой-ой, стоит только указать кому-нибудь на прореху в его мировоззрении, и он сразу обижается.",
-                                        "— И не вини себя.\n— А я и не виню.\n— Значит, ты слушал меня невнимательно."]
-    let sectionNote: [String] = ["New", "Completed"]
+//    let titleArr: [String] = ["Альф про любовь","Альф про еду","Советы от Альфа"]
+//    let descriptionLoveArr: [String] = ["Ей пришла в голову глупейшая мысль, что она меня больше не любит.",
+//                                        "Боюсь, тебе придется любить меня до гроба.",
+//                                        "Я вас, люди, не понимаю. Если вы любите кого-то, вы боитесь, чтоб он узнал об этом. Но, когда оказывается, что вы ему тоже небезразличны, вы жалеете о потерянном времени. А если завтра ваша планета взорвется?"]
+//    let descriptionFoodArr: [String] = ["Твои веки тяжелеют... Ты засыпаешь... Ты больше не кот... Ты бублик.",
+//                                        "Овощи — это не еда, это то, что едят с едой!",
+//                                        "Вы не любите кошек? Да вы просто не умеете их готовить!"]
+//    let descriptionTipsArr: [String] = ["Лин, послушай старших: оставь искусство! Действуй по плану «А» — хватайся за первого попавшегося богача.",
+//                                        "Ой-ой-ой, стоит только указать кому-нибудь на прореху в его мировоззрении, и он сразу обижается.",
+//                                        "— И не вини себя.\n— А я и не виню.\n— Значит, ты слушал меня невнимательно."]
     
+    
+    
+    let sectionNote: [String] = ["New", "Completed"]
+//
     struct Note {
         var title = String()
-        var createdDate = String()
         var description = String()
+        var createdDate = String()
+        var completed = Bool()
     }
     
-    var allNoteArr = [Note]()
+    var newNoteArr = [Note]()
     var selectedNoteArr = [Note]()
     
-    func dateCurrent() -> String {
-        let date = NSDate()
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd.MM hh:mm:ss"
-        return dateFormatter.string(from: date as Date)
-    }
+//    func dateCurrent() -> String {
+//        let date = NSDate()
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "dd.MM hh:mm:ss"
+//        return dateFormatter.string(from: date as Date)
+//    }
     
-    @IBAction func addButton(_ sender: UIButton) {
-        
-        let Rand = Int.random(in: 0...2)
-        var titleInNote, dateInNote, descriptionInNote: String
-        
-        titleInNote = titleArr[Rand]
-        dateInNote = dateCurrent()
-        
-        switch Rand {
-        case 1:
-            descriptionInNote = descriptionFoodArr[Rand]
-        case 2:
-            descriptionInNote = descriptionTipsArr[Rand]
-        default:
-            descriptionInNote = descriptionLoveArr[Rand]
-        }
-
-        let oneNote = Note.init(title: titleInNote, createdDate: dateInNote, description: descriptionInNote)
-        
-        self.addAllNote(oneNote)
-        noteTableView.reloadData()
-    }
-    
-    func addAllNote(_ oneNote: TableNoteViewController.Note) {
-        let index = 0
-        allNoteArr.insert(oneNote, at: index)
-    }
+//    @IBAction func addButton(_ sender: UIButton) {
+//
+//        let Rand = Int.random(in: 0...2)
+//        var titleInNote, dateInNote, descriptionInNote: String
+//
+//        titleInNote = titleArr[Rand]
+//        dateInNote = dateCurrent()
+//
+//        switch Rand {
+//        case 1:
+//            descriptionInNote = descriptionFoodArr[Rand]
+//        case 2:
+//            descriptionInNote = descriptionTipsArr[Rand]
+//        default:
+//            descriptionInNote = descriptionLoveArr[Rand]
+//        }
+//
+//        let oneNote = Note.init(title: titleInNote, createdDate: dateInNote, description: descriptionInNote)
+//
+//        self.addAllNote(oneNote)
+//        noteTableView.reloadData()
+//    }
+//
+//    func addAllNote(_ oneNote: TableNoteViewController.Note) {
+//        let index = 0
+//        newNoteArr.insert(oneNote, at: index)
+//    }
     
     
     
@@ -80,21 +84,22 @@ class TableNoteViewController: UIViewController {
 }
 
 // MARK: - UITableViewDataSource
+
 extension TableNoteViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if section == 0 {
-            return allNoteArr.count
+            return newNoteArr.count
         } else {
             return selectedNoteArr.count
         }
     }
     
-//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-//        let labelSection = UILabel()
-//        labelSection.text = sectionNote[section]
-//        return labelSection.text
-//    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let labelSection = UILabel()
+        labelSection.text = sectionNote[section]
+        return labelSection.text
+    }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let labelSection = UILabel()
@@ -111,7 +116,7 @@ extension TableNoteViewController: UITableViewDataSource, UITableViewDelegate {
         
         var theArr: Note
         if indexPath.section == 0 {
-            theArr = allNoteArr[indexPath.row]
+            theArr = newNoteArr[indexPath.row]
         } else {
             theArr = selectedNoteArr[indexPath.row]
         }
@@ -128,7 +133,7 @@ extension TableNoteViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             if indexPath.section == 0 {
-                allNoteArr.remove(at: indexPath.row)
+                newNoteArr.remove(at: indexPath.row)
                 noteTableView.deleteRows(at: [indexPath], with: .automatic)
             } else {
                 selectedNoteArr.remove(at: indexPath.row)
@@ -139,14 +144,12 @@ extension TableNoteViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.section == 0 {
-            selectedNoteArr.append(allNoteArr[indexPath.row])
-            allNoteArr.remove(at: indexPath.row)
+            selectedNoteArr.append(newNoteArr[indexPath.row])
+            newNoteArr.remove(at: indexPath.row)
         } else {
-            allNoteArr.append(selectedNoteArr[indexPath.row])
+            newNoteArr.append(selectedNoteArr[indexPath.row])
             selectedNoteArr.remove(at: indexPath.row)
         }
         noteTableView.reloadData()
     }
-    
-    
 }
